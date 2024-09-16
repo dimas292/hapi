75453d97271e9db0be6b3b71fa3cad8ef69c75b7
dimas <dimasixc22@gmail.com>
2024-09-16 22:35:28 +0700

    feat : hapi framework

---
+++ src/routes.js
@@ -0,0 +1,21 @@
+const { addNoteHandler, getAllNotesHandler, getNoteByIdHandler } = require('./handler');
+
+const router = [
+  {
+    method: 'POST',
+    path: '/notes',
+    handler: addNoteHandler
+  },
+  {
+    method: 'GET',
+    path: '/notes',
+    handler: getAllNotesHandler
+  },
+  {
+    method: 'GET',
+    path: '/notes/{id}',
+    handler: getNoteByIdHandler
+  }
+];
+
+module.exports = router;
